f026ab0df068b855520688823065e94d51c6aa8a
kaku <kaku981019@gmail.com>
2019-06-16 10:53:20 +0000

    Initialize repository3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "¡Hola, mundo!"
+    render html: "¡Hola, und"
   end
   
   def goodbye
